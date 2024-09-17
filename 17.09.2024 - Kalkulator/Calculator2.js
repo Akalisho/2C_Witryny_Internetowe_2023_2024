ba1f5e6b2e304530c48f7d5c340131d7adf1093d
Micheal Obama <144697131+Akalisho@users.noreply.github.com>
2024-09-17 14:53:57 +0200

    Add files via upload

---
+++ 17.09.2024 - Kalkulator/Calculator2.js
@@ -0,0 +1,15 @@
+let zmienna;
+
+zmienna = prompt("Podaj liczbę pierwszą: ");
+
+let zmienna2;
+
+zmienna2 = prompt("Podaj liczbę drugą: ");
+document.write("Dodawanie: ");
+document.write(zmienna + zmienna2);
+document.write("Odejmowanie: ");
+document.write(zmienna - zmienna2);
+document.write("Mnożenie: ");
+document.write(zmienna * zmienna2);
+document.write("Dzielenie: ");
+document.write(zmienna / zmienna2);
